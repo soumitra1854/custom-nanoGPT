c57154f60a56c8279fa72875a987fe619cd89fce
soumitra <soumitra1854@gmail.com>
2026-02-09 13:00:46 +0530

    custom prompt

--- sample.py
+++ sample.py
@@ -12,7 +12,7 @@ import time
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out' # ignored if init_from is not 'resume'
-start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+start = "FILE:prompt.txt" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
 num_samples = 10 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
@@ -96,5 +96,8 @@ with torch.no_grad():
             print(f"Total time: {total_time:.4f}s")
             print(f"Per-token time: {per_token_time:.6f}s")
             print(f"Peak GPU memory: {peak_mem:.2f} MB")
+            print('---------------')
+            print(f"start context: '''{start}'''")
+            print('---------------')
             print(decode(y[0].tolist()))
             print('---------------')
